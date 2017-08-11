fa038fdc83f51396cbdacf38105dc3886d480c9c
sergej jurecko <sergej.jurecko@wpn.tv>
2017-08-11 07:53:33 +0200

    ..

--- src/butil.erl
+++ src/butil.erl
@@ -2,7 +2,9 @@
 % License, v. 2.0. If a copy of the MPL was not distributed with this
 % file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+-ifndef(MODULED).
 -module(butil).
+-endif.
 -compile(export_all).
 % -define(DEBUG,true).
 -include_lib("../include/bkdcore.hrl").
